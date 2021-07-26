33ecd7738cd334fa52c3073da8294b64b64d6cd3
Jonathan Souza <49069814+JSouza27@users.noreply.github.com>
2021-07-26 12:14:13 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <section className="loading-text">
+      <h1>
+        <span>s</span>
+        <span>l</span>
+        <span>i</span>
+        <span>d</span>
+        <span>o</span>
+      </h1>
+    </section>
+  );
+}
